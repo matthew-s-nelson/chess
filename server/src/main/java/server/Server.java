caf1418cba7044bb95c3042fca9b8ba112a269d3
Matthew Nelson <matthewbuzz@gmail.com>
2024-02-23 09:01:59 -0700

    Adding service class and resources.

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,26 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
+    public static void main(String[] args) {
+        new Server().run(8080);
+    }
+}
